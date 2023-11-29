625ecf09ea642c13533dcb7c2ff9e40e8bee5bca
JustMike <mikelazos.gr@gmail.com>
2023-11-30 01:47:49 +0200

    added print line 15

--- Scripts/PlayerScript/PlayerStateManager.cs
+++ Scripts/PlayerScript/PlayerStateManager.cs
@@ -13,6 +13,7 @@ public partial class PlayerStateManager : CharacterBody3D
 	public override void _Ready()
 	{
 		base._Ready();
+		GD.Print("Hello World");
 		_currState = _walkingState;
 		_currState.Ready(this);
 	}
